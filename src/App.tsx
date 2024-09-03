b32ada4969f3da7c094e680f18c7f9ada8d9ce4f
PavelTitov16 <pashoktitov16@gmail.com>
2024-09-04 02:23:06 +0300

    feat: add test for bot integration

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,8 @@ function App() {
   return (
     <>
       <div className='flex justify-center'>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
