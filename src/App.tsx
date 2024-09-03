627868edf8ab16ee9a20940cdac149ac7686986e
PavelTitov16 <pashoktitov16@gmail.com>
2024-09-03 23:52:02 +0300

    init: prepare project configuration with state manager and ur library

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState, increment, decrement } from './store'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <>
+      <div className='flex justify-center'>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => dispatch(decrement())}>-</button>
+        <span>{count}</span>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <h1 className="text-4xl font-bold underline">
+        Hello Tailwind!
+      </h1>
+    </>
+  )
+}
+
+export default App
