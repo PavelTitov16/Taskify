478af51feed249dd4d87b94dacabd6654bc04d41
PavelTitov16 <pashoktitov16@gmail.com>
2024-09-05 20:18:37 +0300

    refactor: change eslint config

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import eslintPluginReact from 'eslint-plugin-react'
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin'
+import typescriptEslintParser from '@typescript-eslint/parser'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+
+export default [
+  {
+    ignores: ['node_modules', 'dist'],
+  },
+  {
+    files: ['*.ts', '*.tsx'],
+    languageOptions: {
+      parser: typescriptEslintParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslintPlugin,
+      react: eslintPluginReact,
+      'react-hooks': eslintPluginReactHooks,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+  },
+  {
+    files: ['*.js', '*.jsx'], // Для файлов JavaScript
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    plugins: {
+      react: eslintPluginReact,
+      'react-hooks': eslintPluginReactHooks,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+]
