ea3ea6a0eb81a36a43a111b70abb7d2483941cd9
PavelTitov16 <pashoktitov16@gmail.com>
2024-09-09 01:01:36 +0300

    refactor: add new line for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,8 @@ import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
 import typescriptEslintParser from '@typescript-eslint/parser';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 
-export default [{
+export default [
+  {
     ignores: ['node_modules', 'dist'],
   },
   {
