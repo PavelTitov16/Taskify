29554295724fbefdf1a4fe0c2536d814643aa7b0
PavelTitov16 <pashoktitov16@gmail.com>
2024-10-16 23:34:08 +0300

    feat: add color variables to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#FFFFFF',
+        gray3: '#E5E7EB',
+        primary: '#3758F9',
+        primaryText: '#637381',
+        secondaryText: '#8899A8',
+        font: '#231F20',
+        dark: '#111928',
+        pageBg: '#f9fafb',
+        stroke: '#DFE4EA',
+        dark5: '#6B7280',
+      },
+    },
   },
   plugins: [],
 };
