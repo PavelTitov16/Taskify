d004b57889c13f2f49c4dd736e70e6590e0eb3a8
alexey.krylov <turn2river@gmail.com>
2024-10-27 21:22:24 +0300

    feat: change App bg color

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { Footer } from './components/Footer';
 
 export const App = () => {
   return (
-    <div className="app flex flex-col justify-between h-screen bg-gray-300">
+    <div className="app flex flex-col justify-between h-screen bg-gray-100">
       <Header />
       <Router>
         {/* <nav>
